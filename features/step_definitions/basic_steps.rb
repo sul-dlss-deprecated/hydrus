567dc58553742a2068af9016252e1b36507a266a
Naomi Dushay <ndushay@stanford.edu>
2012-05-08 16:49:46 -0700

    wip for show collection view

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,6 +2,12 @@
 Given /^I am on the home page$/ do
   visit "/"
 end
+
+When /^I am viewing "([^"]*)"$/ do |object_id|
+  visit "/catalog/#{object_id}"
+#  pending # express the regexp above with the code you wish you had
+end
+
  
 Then /^I should not see "([^"]*)"$/ do |text|
   page.should_not have_content text
