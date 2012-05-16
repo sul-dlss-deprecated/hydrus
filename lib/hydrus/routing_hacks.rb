cdba0ee454986adde6156be3158bb5df700d70e6
Monty Hindman <montyhindman@gmail.com>
2012-05-15 17:52:37 -0700

    Rationalized the routing hacks.

--- lib/hydrus/routing_hacks.rb
+++ lib/hydrus/routing_hacks.rb
@@ -1,79 +1,42 @@
-# NOTE: Chris Beer is responsible for this code -- his masterpiece!!
+# NOTE: Crazy temporary hacks here. Speak with Chris Beer and Monty Hindman.
 
 module Hydrus::RoutingHacks
 
+  def catalog_path           (*args);  cat_up('',       '_path', *args)  end
+  def catalog_url            (*args);  cat_up('',       '_url',  *args)  end
+  def edit_catalog_path      (*args);  cat_up('edit_',  '_path', *args)  end
+  def edit_catalog_url       (*args);  cat_up('edit_',  '_url',  *args)  end
+  def solr_document_path     (*args);  sdoc_up('',      '_path', *args)  end
+  def solr_document_url      (*args);  sdoc_up('',      '_url',  *args)  end
+  def edit_solr_document_path(*args);  sdoc_up('edit_', '_path', *args)  end
+  def edit_solr_document_url (*args);  sdoc_up('edit_', '_url',  *args)  end
+
+  def cat_up(prefix, suffix, *args)
+    # The catalog _url or _path.
+    doc = args.first
+    case doc
+    when SolrDocument
+      sdoc_up(prefix, '_path', *args)
+    when Hash
+      super
+    else
+      send(prefix + 'polymorphic' + suffix, *args)
+    end
+  end
+
   def sdoc_up(prefix, suffix, *args)
+    # The _url or _path for a SolrDocument.
     doc = args.first
     url_params = {
       :controller => 'catalog',
       :action     => 'show', 
-      :only_path  => suffix == '_url',
+      :only_path  => suffix == '_path',
     }
     case doc
     when Hash
       url_for(url_params.merge doc)
     else
       send(prefix + doc.route_key + suffix, *args)
-    end
-  end
-
-  def solr_document_path(*args)
-    sdoc_up('', '_path', *args)
-  end
-
-  def solr_document_url(*args)
-    sdoc_up('', '_url', *args)
-  end
-
-  def edit_solr_document_path(*args)
-    sdoc_up('edit_', '_path', *args)
-  end
-
-  def edit_solr_document_url(*args)
-    sdoc_up('edit_', '_url', *args)
-  end
-
-  def catalog_path(*args)
-    doc = args.first
-    case doc
-    when SolrDocument
-      sdoc_up('', '_path', *args)
-    when Hash
-      super
-    else
-      polymorphic_path(*args)
-    end
-  end
-
-  def catalog_url(*args)
-    doc = args.first
-    case doc
-    when SolrDocument
-      sdoc_up('', '_url', *args)
-    when Hash
-      super
-    else
-      polymorphic_url(*args)
-    end
-  end
-
-  def edit_catalog_path(*args)
-    doc = args.first
-    case doc
-    when Hash
-      super
-    else
-      edit_polymorphic_path(*args)
-    end
-  end
-
-  def edit_catalog_url(*args)
-    doc = args.first
-    case doc
-    when Hash
-      super
-    else
-      edit_polymorphic_url(*args)
     end
   end
 
