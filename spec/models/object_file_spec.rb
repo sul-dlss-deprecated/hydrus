7a03ef3be8ad2d4260b9dc8965453843a93b90ad
Monty Hindman <montyhindman@gmail.com>
2012-05-25 10:15:06 -0700

    Additional test for object_file.rb

--- spec/models/object_file_spec.rb
+++ spec/models/object_file_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe Hydrus::ObjectFile do
+
   fixtures :object_files
     
   it "should find four files associated with the first item and it should grab the url of a given file" do
     pid = 'druid:oo000oo0001'
     files = Hydrus::ObjectFile.find_all_by_pid(pid)
     files.size.should == 4
-    files[0].url.should == '/uploads/oo/000/oo/0001/pinocchio.htm'
+    f = files[0]
+    exp_url = '/uploads/oo/000/oo/0001/pinocchio.htm'
+    f.url.should == exp_url
+    f.current_path.should == "#{Rails.root}/public#{exp_url}"
     files[1].filename.should == %q{pinocchio characters tc in file name.pdf}
   end
 
