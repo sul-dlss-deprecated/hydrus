d599f1ea6c9f0c84fa6eff71bfcc48476c590509
Monty Hindman <montyhindman@gmail.com>
2012-05-31 11:18:49 -0700

    Added a model to our APO fixtures, and wrote exclude_unwanted_models() to exclude APOs from search results.

--- lib/hydrus/access_controls_enforcement.rb
+++ lib/hydrus/access_controls_enforcement.rb
@@ -3,4 +3,15 @@ module Hydrus::AccessControlsEnforcement
   def apply_gated_discovery solr_parameters, user_parameters
 
   end
+
+  # This filters out objects that you want to exclude from search results.
+  # By default it only excludes FileAssets
+  # @param solr_parameters the current solr parameters
+  # @param user_parameters the current user-subitted parameters
+  def exclude_unwanted_models(solr_parameters, user_parameters)
+    solr_parameters[:fq] ||= [
+      '-has_model_s:"info:fedora/afmodel:Dor_AdminPolicyObject"',
+    ]
+  end
+
 end
