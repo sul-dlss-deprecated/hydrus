13771a1ab09c6378e110ea157d9534fefb58b40e
Naomi Dushay <ndushay@stanford.edu>
2012-06-01 13:17:09 -0700

    added cite-related-as to item edit form, with approp. tests

--- spec/integration/item_edit_spec.rb
+++ spec/integration/item_edit_spec.rb
@@ -39,7 +39,7 @@ describe("Item edit", :type => :request) do
     fill_in "Publisher", :with => orig_name
     click_button "Save"
   end
-
+  
   it "People/Role editing" do
     new_name  = "MY EDITIED PERSON"
     orig_name = "Rosenfeld, Michael J."
@@ -78,9 +78,9 @@ describe("Item edit", :type => :request) do
     orig_link   = "http://www.gutenberg.org/ebooks/500"
     new_link    = "foo_LINK_bar"
     field_link  = "asset_descMetadata_relatedItem_identifier_0"
-    field_title = "asset_descMetadata_relatedItem_titleInfo_title_0"
     orig_title  = "Online survey research site (really Project Gutenberg)"
     new_title   = "foo_TITLE_bar"
+    field_title = "asset_descMetadata_relatedItem_titleInfo_title_0"
 
     login_as_archivist1
 
@@ -105,6 +105,63 @@ describe("Item edit", :type => :request) do
     current_path.should == edit_polymorphic_path(@hi)
     fill_in(field_link,  :with => orig_link)
     fill_in(field_title, :with => orig_title)
+    click_button "Save"
+  end
+
+  it "can edit preferred citation field without affecting cite-related-as citation field" do
+    new_pref_cit = "new preferred citation"
+    orig_pref_cit = 'How Couples Meet and Stay Together ("HCMST": NSF Grant SES-0751613, 2012)'
+    cite_rel_as = "Project Gutenberg citation."
+    
+    login_as_archivist1
+
+    visit edit_polymorphic_path(@hi)
+    current_path.should == edit_polymorphic_path(@hi)
+
+    find_field("preferred_citation").value.should == orig_pref_cit
+    find_field("relatedItem_cite_related_as").value.should == cite_rel_as
+    fill_in "preferred_citation", :with => new_pref_cit
+    click_button "Save"
+    page.should have_content(@notice)
+
+    current_path.should == polymorphic_path(@hi)
+    visit polymorphic_path(@hi)
+    page.should have_content(new_pref_cit)
+    page.should have_content(cite_rel_as)
+
+    # Clean up.
+    visit edit_polymorphic_path(@hi)
+    current_path.should == edit_polymorphic_path(@hi)
+    fill_in "preferred_citation", :with => orig_pref_cit
+    click_button "Save"
+  end
+
+  it "edit cite-related-as without affected preferred-citation" do
+    new_cite_rel_as = "new cite related as"
+    orig_cite_rel_as = "Project Gutenberg citation."
+    cite_rel_as_field = "relatedItem_cite_related_as"
+    pref_cit = 'How Couples Meet and Stay Together ("HCMST": NSF Grant SES-0751613, 2012)'
+    
+    login_as_archivist1
+
+    visit edit_polymorphic_path(@hi)
+    current_path.should == edit_polymorphic_path(@hi)
+
+    find_field(cite_rel_as_field).value.should == orig_cite_rel_as
+    find_field("preferred_citation").value.should == pref_cit
+    fill_in cite_rel_as_field, :with => new_cite_rel_as
+    click_button "Save"
+    page.should have_content(@notice)
+
+    current_path.should == polymorphic_path(@hi)
+    visit polymorphic_path(@hi)
+    page.should have_content(new_cite_rel_as)
+    page.should have_content(pref_cit)
+
+    # Clean up.
+    visit edit_polymorphic_path(@hi)
+    current_path.should == edit_polymorphic_path(@hi)
+    fill_in cite_rel_as_field, :with => orig_cite_rel_as
     click_button "Save"
   end
 
