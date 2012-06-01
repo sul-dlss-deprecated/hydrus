62f943976bfe86992936a3cda33825c3efea19f7
Monty Hindman <montyhindman@gmail.com>
2012-05-31 17:45:16 -0700

    Added stub for test for search results page (Naomi will finish)

---
+++ spec/integration/index_page_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe("Search page", :type => :request) do
+
+  it "Some results should appear" do
+    # puts catalog_path
+    # page.should have_content("xxxxxxxxxxxxxxx")
+  end
+
+end
