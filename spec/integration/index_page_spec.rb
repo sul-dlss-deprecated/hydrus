9ab38c9e35a1da15e36d3da33f1a41950e0fa936
Naomi Dushay <ndushay@stanford.edu>
2012-06-01 13:30:58 -0700

    added basic spec for search results page for archivist1

--- spec/integration/index_page_spec.rb
+++ spec/integration/index_page_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe("Search page", :type => :request) do
 
-  it "Some results should appear" do
-    # puts catalog_path
-    # page.should have_content("xxxxxxxxxxxxxxx")
+  it "Some results should appear for archivist1 user for default search" do
+    visit root_path
+
+    login_as_archivist1
+    click_button("search")
+
+    page.should have_content("Object ID")
+    page.should have_content("druid:oo000oo0001")
+    page.should have_content("Hydrus_Item")
+    page.should have_content("druid:oo000oo0003")
+    page.should have_content("Hydrus_Collection")
   end
 
 end
