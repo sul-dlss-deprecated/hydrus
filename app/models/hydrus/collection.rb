34db61454c33e04f76b7a6d57f0e83b7a4afa800
Naomi Dushay <ndushay@stanford.edu>
2012-06-04 09:45:02 -0700

    working on collection object view

--- app/models/hydrus/collection.rb
+++ app/models/hydrus/collection.rb
@@ -1,3 +1,15 @@
 class Hydrus::Collection < Hydrus::GenericObject
   
+  # TODO:  not working properly yet!
+  def related_items
+puts descMetadata.find_by_terms(:relatedItem).inspect
+    @related_items ||= descMetadata.find_by_terms(:relatedItem).collect {|rel_node| 
+      title_node = rel_node.at_css('titleInfo title')
+      url_node = rel_node.at_css('identifier')
+      url = url_node ? url_node.content : nil
+      link_label = title_node ? title_node.content : url
+      Hydrus::RelatedItem.new(:title => link_label, :url => url)
+    }
+  end
+
 end
