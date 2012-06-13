b9cfc08cde3c56b0579768fbad56ae6a79b29ab0
Jessie Keck <jessie.keck@gmail.com>
2012-06-13 13:57:47 -0700

    Changing default request handler to standard instead of search so we can properly register our objects w/ Dor::Serivces.

--- app/models/hydrus/collection.rb
+++ app/models/hydrus/collection.rb
@@ -2,7 +2,7 @@ class Hydrus::Collection < Hydrus::GenericObject
   
   def hydrus_items
     query = %Q(is_member_of_collection_s:"info:fedora/#{pid}")
-    resp  = Blacklight.solr.find('q.alt'.to_sym => query)
+    resp  = Blacklight.solr.find('q'.to_sym => query)
     items = resp.docs.map { |d| Hydrus::Item.find(d.id) }
     return items
   end
