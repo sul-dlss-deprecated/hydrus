e931b70d72d244a9324924007ac9302ed56b2e36
Jessie Keck <jessie.keck@gmail.com>
2012-06-15 13:42:22 -0700

    Improving HydrusCollectionControllerSpec.

--- spec/controllers/hydrus_collections_controller_spec.rb
+++ spec/controllers/hydrus_collections_controller_spec.rb
@@ -17,4 +17,25 @@ describe HydrusCollectionsController do
 
   end
 
+  describe "update action" do
+    before(:all) do
+      @pid = "druid:oo000oo0003"
+    end
+    it "should not allow a user to update an object if you do not have edit permissions" do
+      controller.stub(:current_user).and_return(mock_user)
+      put :update, :id => @pid
+      assigns[:document_fedora].should be_nil
+      response.should redirect_to hydrus_collection_path(@pid)
+      flash[:notice].should =~ /You do not have sufficient privileges to edit this document/
+      flash[:notice].should =~ /You have been redirected to the read-only view/
+    end
+    it "should allow a user to update an object if they do have edit permissions" do
+      controller.stub(:current_user).and_return(mock_authed_user)
+      put :update, :id => @pid
+      assigns[:document_fedora].should_not be_nil
+      response.should redirect_to hydrus_collection_path(@pid)
+      flash[:notice].should =~ /Your changes have been saved/
+    end
+  end
+
 end
