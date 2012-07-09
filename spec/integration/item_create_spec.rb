3b187f11a720211e124edf767068ee1365fa0afb
Monty Hindman <montyhindman@gmail.com>
2012-07-09 14:12:04 -0700

    Wrote integration test for create-new-Item via Collection page

--- spec/integration/item_create_spec.rb
+++ spec/integration/item_create_spec.rb
@@ -2,41 +2,52 @@ require 'spec_helper'
 
 describe("Item create", :type => :request, :integration => true) do
 
-  before :all do
+  before(:all) do
     @notice = "Your changes have been saved."
+    @hc_druid = 'druid:oo000oo0003'
+    @edit_path_regex = Regexp.new('/items/(druid:\w{11})/edit')
+    # Need to mint an actual druid in order to pass validation.
+    @prev_mint_ids = Dor::Config.configure.suri.mint_ids
+    Dor::Config.configure.suri.mint_ids = true
+  end
+
+  after(:all) do
+    # Restore mint_ids setting.
+    Dor::Config.configure.suri.mint_ids = @prev_mint_ids
   end
 
   it "should be able to create a new Item" do
-    # Need to mint an actual druid in order to pass validation.
-    prev_mint_ids = Dor::Config.configure.suri.mint_ids
-    Dor::Config.configure.suri.mint_ids = true
-    # Login, go to new Item page, and save the druid.
+    # Login, go to new Item page, and store the druid of the new Item.
     login_as_archivist1
-    visit new_hydrus_item_path(:collection => 'druid:oo000oo0003')
-    path_regex = Regexp.new('/items/(druid:\w{11})/edit')
-    current_path.should =~ path_regex
-    druid = path_regex.match(current_path)[1]
+    visit new_hydrus_item_path(:collection => @hc_druid)
+    current_path.should =~ @edit_path_regex
+    druid = @edit_path_regex.match(current_path)[1]
     # Fill in form and save.
     fill_in "Title of item", :with => 'title_foo'
     fill_in "hydrus_item_person_0", :with => 'person_foo'
     fill_in "Abstract", :with => 'abstract_foo'
     click_button "Save"
     page.should have_content(@notice)
-    # Get Item out of fedora and confirm that our edits made it.
+    # Get Item out of fedora and confirm that our edits were persisted.
     item = Hydrus::Item.find(druid)
     item.title.first.should == 'title_foo'
     item.person.first.should == 'person_foo'
     item.abstract.first.should == 'abstract_foo'
     item.should be_instance_of Hydrus::Item
-    # Delete object and restore mint_ids setting.
+    # Delete object.
     item.delete
-    Dor::Config.configure.suri.mint_ids = prev_mint_ids
   end
 
-  # Another test
-  #   - don't fill in required items
-
-  # Another test
-  #   visit via click button from Collection screen
+  it "should be able to access create-new-Item screen via the Collection page" do
+    login_as_archivist1
+    collection = Hydrus::Collection.find(@hc_druid)
+    visit polymorphic_path(collection)
+    click_link('item')
+    current_path.should =~ @edit_path_regex
+    # Delete object.
+    druid = @edit_path_regex.match(current_path)[1]
+    Hydrus::Item.find(druid).delete
+    # Other editing functionality is tested elsewhere.
+  end
 
 end
