7f8d304281d76cbd1ea0b3686bcbb299042c27ca
Monty Hindman <montyhindman@gmail.com>
2012-07-10 16:22:06 -0700

    Improved brittle intration test

--- spec/integration/collection_model_spec.rb
+++ spec/integration/collection_model_spec.rb
@@ -9,7 +9,8 @@ describe Hydrus::Collection do
     druid = 'druid:oo000oo0003'
     hc    = Hydrus::Collection.find druid
     items = hc.hydrus_items
-    items.map { |i| i.pid }.sort.should == %w(1 5 6 7).map { |n| "druid:oo000oo000#{n}" }
+    druids = items.map { |i| i.pid }
+    %w(1 5 6 7).each { |n| druids.should include("druid:oo000oo000#{n}") }
   end
 
   it "should behave nicely when it has no member Hydrus::Items" do
