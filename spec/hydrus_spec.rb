5527b4f50a5f153c59619353f75bdee4b59bc093
Monty Hindman <montyhindman@gmail.com>
2012-07-13 13:29:24 -0700

    Added message to ap_dump().

--- spec/hydrus_spec.rb
+++ spec/hydrus_spec.rb
@@ -4,11 +4,14 @@ require 'stringio'
 describe "ap_dump()" do
   
   it "should write some stuff to the given file handle" do
-    d = [0, 1, 2]
+    message     = 'blah blah'
+    data        = [1111, 2222]
     file_handle = StringIO.new
-    Hydrus.ap_dump(d, file_handle)
+    Hydrus.ap_dump(message, data, file_handle)
     s = file_handle.string
+    s.should include(message)
     s.should =~ /^=====/
+    data.each { |d| s.should include(d.to_s) }
     s.should =~ /=====$/
   end
 
