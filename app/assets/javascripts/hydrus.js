fe60d60aa972393ad22fed456ef13d13f9a01253
Peter Mangiafico <peter@mangiafico.org>
2012-07-18 13:48:12 -0700

    split up hydrus.js file into multiple functions, and only call needed functions for the page we are on

--- app/assets/javascripts/hydrus.js
+++ app/assets/javascripts/hydrus.js
@@ -1,19 +1,46 @@
-$(document).ready(function(){
-	$("#add_person, #add_link, #add_related_citation").click(function(){
-		var button = $(this);
-		var type = $(this).attr("class");
-		var form = $(this).closest("form");
-		var method = $(this).closest("form").attr("method");
-		if($("input[name=_method]",form).length > 0){
-			method = $("input[name=_method]").attr("value");
-		}
-		$.ajax({
-			type: method,
-			url: form.attr("action") + "?format=js&" + button.attr("data-attribute") + "=" + button.attr("value")
-		});
-		return false;
-	});
+function collection_edit_init(){
 	
+	// this method is called when the collection edit page is fully loaded
+	validate_hydrus_collection();
+	activate_edit_controls();
+	$("#hydrus_collections-edit form input, #hydrus_collections-edit form textarea").live("blur", function(){
+	    validate_hydrus_collection();
+	});	
+	
+	// Manage state of select dropdowns when the select should be enabled only when its associated radio button is selected
+	$('div.radio-select-group input:radio').click(function() { // when a radio button in the group is clicked
+		$('div.radio-select-group select').prop('disabled', true); // disable all the selects
+		$('div.radio-select-group input:radio:checked').parentsUntil('.radio-select-option').siblings('div.radio-label').children('select')
+			.prop('disabled', false); // re-enable the select that is associated with the selected radio button
+	});
+	// On page load, execute the code block above to disable appropriate select dropdowns
+	$('div.radio-select-group input:radio:checked').trigger('click');	
+	
+}
+
+function item_edit_init(){
+	
+	// this method is called when the item edit page is fully loaded
+  validate_hydrus_item();
+	activate_edit_controls();
+	$(".terms_of_deposit").each(function(){
+		$(this).click(function(){
+			validate_hydrus_item();
+		});
+	});
+	$("#hydrus_items-edit form input, #hydrus_items-edit form textarea").live("blur", function(){
+	    validate_hydrus_item();
+	});
+}
+
+// this is loaded on each page
+$(document).ready(function(){
+
+	if ($('#hydrus_collections-edit').length == 1) {collection_edit_init();}
+	if ($('#hydrus_items-edit').length == 1) {item_edit_init();}
+
+	$(".abstract").truncate({max_length: 350});
+
 	$("[rel=tooltip]").tooltip();
 	
 	// Modal sign in
@@ -23,19 +50,11 @@ $(document).ready(function(){
 			return false;
 	  });
 	});
-
-	$(".abstract").truncate({max_length: 350});
-
-	// Manage state of select dropdowns when the select should be enabled only when its associated radio button is selected
-	$('div.radio-select-group input:radio').click(function() { // when a radio button in the group is clicked
-		$('div.radio-select-group select').prop('disabled', true); // disable all the selects
-		$('div.radio-select-group input:radio:checked').parentsUntil('.radio-select-option').siblings('div.radio-label').children('select')
-			.prop('disabled', false); // re-enable the select that is associated with the selected radio button
-	});
-	// On page load, execute the code block above to disable appropriate select dropdowns
-	$('div.radio-select-group input:radio:checked').trigger('click');
-
 	
+});
+
+function activate_edit_controls() {
+
 	$(".delete-node").live("click", function(){
 		var button = $(this);
 		$.ajax({url: button.attr("href") + "&format=js",
@@ -55,23 +74,23 @@ $(document).ready(function(){
 		$("form.step").append("<input type='hidden' value='" + $(this).attr('value') + "' name='" + $(this).attr('name') + "' />");	
   	$("form.step").submit();
 	});
-  
-  validate_hydrus_item();
-  validate_hydrus_collection();
 
-	$("#hydrus_items-edit form input, #hydrus_items-edit form textarea").live("blur", function(){
-	    validate_hydrus_item();
-	});
-	$("#hydrus_collections-edit form input, #hydrus_collections-edit form textarea").live("blur", function(){
-	    validate_hydrus_collection();
-	});	
-	$(".terms_of_deposit").each(function(){
-		$(this).click(function(){
-			validate_hydrus_item();
+	$("#add_person, #add_link, #add_related_citation").click(function(){
+		var button = $(this);
+		var type = $(this).attr("class");
+		var form = $(this).closest("form");
+		var method = $(this).closest("form").attr("method");
+		if($("input[name=_method]",form).length > 0){
+			method = $("input[name=_method]").attr("value");
+		}
+		$.ajax({
+			type: method,
+			url: form.attr("action") + "?format=js&" + button.attr("data-attribute") + "=" + button.attr("value")
 		});
-	});
-	
-});
+		return false;
+	});	
+}
+
 function validate_hydrus_item() {
 	var all_required_filled = true;
 	$("#hydrus_items-edit form input:required, #hydrus_items-edit form textarea:required").each(function(){
@@ -92,6 +111,7 @@ function validate_hydrus_item() {
 		});
 	}
 }
+
 function validate_hydrus_collection() {
 	var all_required_filled = true;
 	$("#hydrus_collections-edit form input:required, #hydrus_collections-edit form textarea:required").each(function(){
