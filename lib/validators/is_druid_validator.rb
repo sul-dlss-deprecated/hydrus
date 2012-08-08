aa62b4eac3fd5ea56a90c2326c1f4be5184d481a
Monty Hindman <montyhindman@gmail.com>
2012-08-08 14:35:09 -0700

    Removed extraneous conditional from IsDruid validator

--- lib/validators/is_druid_validator.rb
+++ lib/validators/is_druid_validator.rb
@@ -1,15 +1,8 @@
-# We need a custom validator so we can be sure for required items
-# that we don't have a one element array with a single blank element.
-
 class IsDruidValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    # What is the purpose of the if clause?
-    if value.blank? || !value.downcase.include?('druid')
-      unless DruidTools::Druid.valid?(value)
-        record.errors[attribute] << (options[:message] || "is not a valid druid") 
-      end
-    end
+    return if DruidTools::Druid.valid?(value)
+    record.errors[attribute] << (options[:message] || "is not a valid druid") 
   end
 
 end
