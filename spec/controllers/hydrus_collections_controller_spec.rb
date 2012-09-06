06ee8e9adc62052c5a94d44823289d4b61253d01
Monty Hindman <montyhindman@gmail.com>
2012-09-05 17:28:10 -0700

    Slow unit tests: removed unneeded test -- duplicate of existing integration test

--- spec/controllers/hydrus_collections_controller_spec.rb
+++ spec/controllers/hydrus_collections_controller_spec.rb
@@ -51,9 +51,11 @@ describe HydrusCollectionsController do
   end
 
   describe "Update Action" do
+
     before(:all) do
       @pid = "druid:oo000oo0003"
     end
+
     it "should not allow a user to update an object if you do not have edit permissions" do
       controller.stub(:current_user).and_return(mock_user)
       put :update, :id => @pid
@@ -61,13 +63,7 @@ describe HydrusCollectionsController do
       flash[:alert].should =~ /You do not have sufficient privileges to edit this document/
       flash[:alert].should =~ /You have been redirected to the read-only view/
     end
-    it "should allow a user to update an object if they do have edit permissions" do
-      controller.stub(:current_user).and_return(mock_authed_user)
-      put :update, :id => @pid
-      assigns[:document_fedora].should_not be_nil
-      response.should redirect_to hydrus_collection_path(@pid)
-      flash[:notice].should =~ /Your changes have been saved/
-    end
+
   end
 
 end
