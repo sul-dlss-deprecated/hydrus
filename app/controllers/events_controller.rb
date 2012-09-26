c89562027b03fb2db5c460d0a11f2ed2e3cee6d2
Jessie Keck <jessie.keck@gmail.com>
2012-09-26 16:43:07 -0700

    Enforce permissions on events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,24 @@
 class EventsController < ApplicationController
   
-  include Hydrus::AccessControlsEnforcement
-  
-  #prepend_before_filter :sanitize_update_params, :only => :update
+  include Hydra::AccessControlsEnforcement
   before_filter :enforce_access_controls
-  before_filter :redirect_if_not_correct_object_type, :only => [:edit,:show,:update]
   
   def index
     contextual_id = params.select{|k,v| k.to_s =~ /^hydrus_.*_id/}.each_value.first
-    @document_fedora = ActiveFedora::Base.find(contextual_id, :cast=>true)
-    @document_fedora.current_user = current_user
+    unless contextual_id.blank?
+      @document_fedora = ActiveFedora::Base.find(contextual_id, :cast=>true)
+      @document_fedora.current_user = current_user
+    end
+  end
+  
+  protected
+  
+  def enforce_index_permissions
+    contextual_id = params.select{|k,v| k.to_s =~ /^hydrus_.*_id/}.each_value.first
+    if contextual_id.blank? or !can?(:read, contextual_id)
+      flash[:error] = "You do not have sufficient privileges to read that document."
+      redirect_to root_path
+    end
   end
   
 end
---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe EventsController do
+  it "should restrict access to users who shouldn't be able to view items" do
+    controller.stub(:current_user).and_return(mock_user)
+    get :index
+    flash[:error].should =~ /You do not have sufficient privileges to read that document/
+    response.should redirect_to(root_path)
+  end
+  it "should assign the given object for the given ID as the document_fedora instance variable" do
+    controller.stub(:current_user).and_return(mock_authed_user)
+    mock_coll = mock("HydrusCollection")
+    mock_coll.should_receive(:"current_user=").and_return("")
+    ActiveFedora::Base.should_receive(:find).with("1234", :cast=>true).and_return(mock_coll)
+    get :index, :hydrus_collection_id=>"1234"
+    response.should be_success
+    assigns(:document_fedora).should == mock_coll
+  end
+end
