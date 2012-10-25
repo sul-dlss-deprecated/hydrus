36249f860845810f9726b9271f5c34517682474e
Monty Hindman <montyhindman@gmail.com>
2012-10-25 10:31:22 -0700

    Fixed integration test for search page

--- spec/integration/search_page_spec.rb
+++ spec/integration/search_page_spec.rb
@@ -4,14 +4,11 @@ describe("Search page", :type => :request, :integration => true) do
 
   it "Some results should appear for archivist1 user for default search" do
     visit root_path
-
     login_as_archivist1
     click_button("search")
-
-    page.should have_content("Object ID")
-    page.should have_content("Item")
-    page.should have_content("druid:oo000oo000")
-    page.should have_content("Collection")
+    docs = find 'div#documents'
+    docs.should have_content 'How Couples Meet'
+    docs.should have_content 'SSDS Social Science Data Collection'
   end
 
 end
