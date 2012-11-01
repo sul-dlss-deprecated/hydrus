b0d5f47c997ef733a6389fe14d1aa829d5b998c8
Monty Hindman <montyhindman@gmail.com>
2012-11-01 14:24:08 -0700

    cleanup_usernames(): Refactored for testability, and wrote test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,15 @@
 class User < ActiveRecord::Base
-# Connects this user object to Hydra behaviors. 
- include Hydra::User
-# Connects this user object to Blacklights Bookmarks and Folders. 
- include Blacklight::User
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+
+  include Hydra::User
+  include Blacklight::User
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
 
-  # Method added by Blacklight; Blacklight uses #to_s on your
-  # user class to get a user-displayable login/identifier for
-  # the account. 
+  # Blacklight uses #to_s on your user class to get
+  # a user-displayable login/identifier for the account. 
   def to_s
     return sunetid
   end
@@ -26,4 +21,5 @@ class User < ActiveRecord::Base
   def is_webauth?
     false
   end
+
 end
