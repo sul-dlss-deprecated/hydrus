01fa1780d8978f859ec092c4f71839a25fc05251
Monty Hindman <montyhindman@gmail.com>
2012-11-16 18:22:34 -0800

    Planning work for datetime refactor

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module Hydrus
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # config.time_zone = 'Pacific Time (US & Canada)'
+    config.time_zone = 'UTC'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -55,8 +56,6 @@ module Hydrus
     config.assets.enabled = true    
     # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
     config.assets.compress = !Rails.env.development?
-
-    config.time_zone='UTC'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
