625f43ca8c60c59c36a4cac76d35c60247248f67
Chris Beer <cabeer@stanford.edu>
2013-10-10 08:22:23 -0700

    Solrizer no longer uses the logger.

--- config/boot.rb
+++ config/boot.rb
@@ -3,10 +3,3 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
-
-# Override Solrizer logger before it gets a chance to load and pollute STDERR.
-require 'solrizer'
-solr_log = File.expand_path(File.join(File.dirname(__FILE__), '..', 'log', "solrizer.log"))
-Solrizer.logger = Logger.new solr_log
-Solrizer.logger.level = Logger::INFO
-
