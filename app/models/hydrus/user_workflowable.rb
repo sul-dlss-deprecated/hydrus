9476184e6b10e91912963176005e88bc7ca50ed1
Chris Beer <cabeer@stanford.edu>
2013-10-11 22:04:11 -0700

    check that the object_status is even set before checking its value.

--- app/models/hydrus/user_workflowable.rb
+++ app/models/hydrus/user_workflowable.rb
@@ -2,7 +2,7 @@ module Hydrus::UserWorkflowable
 
   # Returns true if the object is in the draft state.
   def is_draft
-    return object_status == 'draft'
+    return object_status.nil? || object_status == 'draft'
   end
   
   # Returns true if the object status is any flavor of published. This status
@@ -11,7 +11,7 @@ module Hydrus::UserWorkflowable
   # publish_time focuses on the time the object achieves the published
   # milestone in common accessioning.
   def is_published
-    return object_status[0..8] == 'published'
+    return object_status && object_status[0..8] == 'published'
   end
   
   # Returns true if all required fields are filled in.
