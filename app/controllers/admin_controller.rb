6fde49214b4aad17f02a412240d8b8e8bccfb9ff
Peter Mangiafico <peter@mangiafico.org>
2014-01-09 13:15:26 -0800

    restore update_users view to correct location; fix action

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,27 +1,24 @@
 class AdminController < ApplicationController
 
   before_filter :authenticate_user!
+  skip_authorization_check :only => [:update_users] # we check if they are admins in the action itself
 
   # updating the users can only be an ajax call from an administrator
   def update_users
-    return unless request.xhr? && Hydrus::Authorizable.can_act_as_administrator(current_user)
-        
-    # admins=UserRole.find_by_role('administrators')
-    # unless admins.blank?
-    #   admins.users=params[:administrators]
-    #   admins.save
-    # end
-    collection_creators=UserRole.find_by_role('collection_creators')
-    unless collection_creators.blank?
-      collection_creators.users=params[:collection_creators]
-      collection_creators.save
+    unless request.xhr? && Hydrus::Authorizable.can_act_as_administrator(current_user)
+      return 
+    else      
+      collection_creators=UserRole.find_by_role('collection_creators')
+      unless collection_creators.blank?
+        collection_creators.users=params[:collection_creators]
+        collection_creators.save
+      end
+      global_viewers=UserRole.find_by_role('global_viewers')
+      unless global_viewers.blank?
+        global_viewers.users=params[:global_viewers]
+        global_viewers.save
+      end
     end
-    global_viewers=UserRole.find_by_role('global_viewers')
-    unless global_viewers.blank?
-      global_viewers.users=params[:global_viewers]
-      global_viewers.save
-    end
-    
-    render 'update_users.js'
   end
+
 end
