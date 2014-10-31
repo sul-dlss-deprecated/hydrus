3189762543ab35696e6299eb17caf148b65ef1a6
Peter Mangiafico <peter@mangiafico.org>
2014-10-31 10:15:02 -0700

    set less verbose log level in test to create smaller files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,7 @@ Hydrus::Application.configure do
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
+  config.log_level = :warn
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
