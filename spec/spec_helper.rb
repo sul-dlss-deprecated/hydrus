9b32939b7e467d51c02868f288964af86384b881
Justin Coyne <jcoyne@justincoyne.com>
2017-03-09 20:45:29 -0800

    Start stubbing out integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,12 @@ require 'tempfile'
 require 'rspec/matchers' # req by equivalent-xml custom matcher `be_equivalent_to`
 require 'equivalent-xml'
 require 'equivalent-xml/rspec_matchers'
+$: << 'spec/support'
+require 'service_mocks'
+
+# prevent Test::Unit's AutoRunner from executing during RSpec's rake task
+# See https://github.com/rspec/rspec-rails/issues/1171
+Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -73,6 +79,7 @@ RSpec.configure do |config|
 
   # Restore prior state of Fedora repository.
   config.around(:each) do |example|
+    ServiceMocks.mock        
     ActiveFedora::Base.connection_for_pid(0).transaction do |t|
       example.call
       # TODO: simplify if rollback_fixtures() is incorporated into Rubydora.
@@ -83,7 +90,6 @@ RSpec.configure do |config|
       end
     end
   end
-
 end
 
 Dor::Config.configure.suri.mint_ids = false
