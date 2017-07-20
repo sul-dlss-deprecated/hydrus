80862e5438c58af18fdfb319cbe69e832212c7be
Justin Coyne <jcoyne@justincoyne.com>
2017-07-20 13:18:02 -0500

    Update to new Capistrano git configuration. Fixes #81

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,6 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/home/lyberadmin/hydrus'
 
-# Default value for :scm is :git
-# set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
