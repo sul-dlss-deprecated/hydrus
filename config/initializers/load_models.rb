fd86cf81213ed5e37ba4f44c8f2eb54ec66249e8
Chris Beer <chris@cbeer.info>
2017-07-21 08:19:55 -0700

    Explicitly require the rest of the hydrus models

--- config/initializers/load_models.rb
+++ config/initializers/load_models.rb
@@ -1,3 +1,5 @@
 # Dor-services already has defined the model constants, so we need to
 # explicitly require them here
-require 'app/models/hydrus/collection.rb'
+require_dependency Rails.root + 'app/models/hydrus/collection.rb'
+require_dependency Rails.root + 'app/models/hydrus/item.rb'
+require_dependency Rails.root + 'app/models/hydrus/admin_policy_object.rb'
