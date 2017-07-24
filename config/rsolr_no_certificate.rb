a05c056eca84188fcf10b5b8bc0f8b040606ff28
Justin Coyne <jcoyne@justincoyne.com>
2017-07-24 09:37:29 -0500

    Remove monkeypatch of dor-services

--- config/rsolr_no_certificate.rb
+++ config/rsolr_no_certificate.rb
@@ -1,12 +1,3 @@
-# Override make_solr_connection() so that we don't need certs in dev and test.
-module Dor
-  class Configuration
-    def make_solr_connection(add_opts = {})
-      opts = Config.solrizer.opts.merge(add_opts).merge(:url => Config.solrizer.url)
-      ::RSolr.connect(opts).extend(RSolr::Ext::Client)
-    end
-  end
-end
 class RSolr::Connection
   def execute client, request_context
     if request_context[:method] != :post
