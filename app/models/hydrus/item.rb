7386d7c4d124d2d351abc50382329634c4b2cf43
Chris Beer <chris@cbeer.info>
2017-07-25 15:04:12 -0700

    Add overridden APO relation to items as well

--- app/models/hydrus/item.rb
+++ app/models/hydrus/item.rb
@@ -3,6 +3,8 @@ class Hydrus::Item < Hydrus::GenericObject
   include Hydrus::Responsible
   include Hydrus::EmbargoMetadataDsExtension
 
+  # Override Dor::Governable so that we look for Hydrus::AdminPolicyObjects
+  belongs_to :admin_policy_object, property: :is_governed_by, class_name: 'Hydrus::AdminPolicyObject'
   REQUIRED_FIELDS = [:title, :abstract, :contact, :keywords, :version_description, :date_created]
 
   after_validation :strip_whitespace
