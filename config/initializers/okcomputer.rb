d8a57fed18f0c9a8b1eaee3ec6c50d1aaaefb0f6
Justin Coyne <jcoyne@justincoyne.com>
2017-07-26 16:26:58 -0500

    Add an okcomputer check for client cert connections
    Fixes #114
    
    Also add userinfo to the Suri connection.

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,4 +1,56 @@
 require 'okcomputer'
+require 'faraday'
+# Check an HTTP connection with client certificates
+class HttpWithClientKeyCheck < OkComputer::Check
+  ConnectionFailed = Class.new(StandardError)
+
+  attr_accessor :url, :request_timeout
+
+  # Public: Initialize a new HTTP check.
+  #
+  # url - The URL to check
+  # request_timeout - How long to wait to connect before timing out. Defaults to 5 seconds.
+  def initialize(url, request_timeout = 5)
+    self.url = url
+    self.request_timeout = request_timeout
+  end
+
+  # TODO: it would be great if there was a method in dor-services for this
+  def client_cert
+    Dor::Config.fedora.client.options[:ssl_client_cert]
+  end
+
+  # TODO: it would be great if there was a method in dor-services for this
+  def client_key
+    Dor::Config.fedora.client.options[:ssl_client_key]
+  end
+
+  def connection
+    Faraday::Connection.new url, ssl: { client_cert: client_cert,
+                                        client_key: client_key }
+  end
+
+  def resource
+    Timeout.timeout(request_timeout) do
+      connection.get(url)
+    end
+  rescue => e
+    raise ConnectionFailed, e
+  end
+
+  # Public: Return the status of the HTTP check
+  def check
+    if resource.status == 200
+      mark_message "HTTP check successful"
+    else
+      mark_message "Error: Status code is #{resource.status}"
+      mark_failure
+    end
+  rescue => e
+    mark_message "Error: '#{e}'"
+    mark_failure
+  end
+end
 
 # /status for 'upness' (Rails app is responding), e.g. for load balancer
 # /status/all to show all dependencies
@@ -15,11 +67,12 @@ solr_url = Dor::Config.solrizer.url + "/select"
 OkComputer::Registry.register 'solr_url', OkComputer::HttpCheck.new(solr_url)
 
 # fedora
-OkComputer::Registry.register 'fedora_url', OkComputer::HttpCheck.new(Dor::Config.fedora.url)
+OkComputer::Registry.register 'fedora_url', HttpWithClientKeyCheck.new(Dor::Config.fedora.url)
 
 # suri
-suri_url = Dor::Config.suri.url + '/suri2/namespaces'
-OkComputer::Registry.register 'suri_url', OkComputer::HttpCheck.new(suri_url)
+suri_url = URI(Dor::Config.suri.url + '/suri2/namespaces')
+suri_url.userinfo = [Dor::Config.suri.user, Dor::Config.suri.pass].join(':')
+OkComputer::Registry.register 'suri_url', OkComputer::HttpCheck.new(suri_url.to_s)
 
 # dor-services-app
 about_url = Dor::Config.dor_services.url + '/v1/about'
