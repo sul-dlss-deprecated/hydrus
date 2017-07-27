fee023508245e71d5d147e33aef79cd2e4deff74
Justin Coyne <jcoyne@justincoyne.com>
2017-07-27 14:14:11 -0500

    Get rid of empty module EmbargoMetadataDsExtension

---
+++ config/initializers/dor_services_monkeypatches.rb
@@ -0,0 +1,5 @@
+# It would be best if we could find an alternative way doing this.
+# Monkeypatches are rarely a good idea.
+
+Dor::EmbargoMetadataDS.include Hydrus::GenericDS
+Dor::EmbargoMetadataDS.include Hydrus::Accessible
