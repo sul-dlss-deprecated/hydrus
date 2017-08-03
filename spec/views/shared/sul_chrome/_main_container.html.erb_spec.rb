ba6f4d6c1c65dd215030b2c25215b37039b051dc
Justin Coyne <jcoyne@justincoyne.com>
2017-08-03 09:23:15 -0700

    Add test for the test version ribbon
    Use the correct variable too (as set in config/settings.yml)

---
+++ spec/views/shared/sul_chrome/_main_container.html.erb_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'shared/sul_chrome/_main_container.html.erb' do
+  before do
+    allow(Settings.hydrus).to receive(:show_test_banner?).and_return(banner)
+    render
+  end
+
+  context 'when test banner is on' do
+    let(:banner) { true }
+
+    it 'shows the banner' do
+      expect(rendered).to have_selector 'img[src="/assets/test-version-ribbon.png"]'
+    end
+  end
+
+  context 'when test banner is off' do
+    let(:banner) { false }
+
+    it "doesn't show the banner" do
+      expect(rendered).not_to have_selector 'img[src="/assets/test-version-ribbon.png"]'
+    end
+  end
+end
