82a64184584c05810f2b0dccf8aabfa7e92097e6
Peter Mangiafico <peter@mangiafico.org>
2017-09-08 09:41:09 -0700

    remove dev deploy stage

--- config/deploy/dev.rb
+++
@@ -1,6 +0,0 @@
-server 'hydrus-dev.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'development'
-
-set :bundle_without, %w{test deployment}.join(' ')
