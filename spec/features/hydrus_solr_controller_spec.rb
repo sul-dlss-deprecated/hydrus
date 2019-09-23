1f011ccdcdf19731b6a1d082c3dfba3a4882e04d
Justin Coyne <jcoyne@justincoyne.com>
2019-09-23 10:58:50 -0500

    Avoid using the deprecated SearchService.solr method

--- spec/features/hydrus_solr_controller_spec.rb
+++ spec/features/hydrus_solr_controller_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe(HydrusSolrController, type: :feature, integration: true) do
   describe '#delete_from_index' do
     it 'removes items from solr' do
-      expect(Dor::SearchService.solr).to receive(:delete_by_id).with('x')
+      expect(ActiveFedora.solr.conn).to receive(:delete_by_id).with('x')
       visit '/hydrus_solr/delete_from_index/x'
     end
   end
 
   describe '#reindex' do
     it 'indexes an item into solr' do
-      expect(Dor::SearchService.solr).to receive(:add).with(hash_including(id: 'druid:oo000oo0001'), anything)
+      expect(ActiveFedora.solr.conn).to receive(:add).with(hash_including(id: 'druid:oo000oo0001'), anything)
       visit '/hydrus_solr/reindex/druid:oo000oo0001'
     end
   end
