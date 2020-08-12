14badfdb4a3369c6775ad15c787915e2e7b8f182
aaron-collier <aaron.collier@stanford.edu>
2020-08-12 15:29:20 -0700

    Enable webmock properly

--- app/services/workflow_client_factory.rb
+++ app/services/workflow_client_factory.rb
@@ -4,6 +4,6 @@
 class WorkflowClientFactory
   def self.build
     logger = Logger.new(Settings.workflow.logfile, Settings.workflow.shift_age)
-    Dor::Workflow::Client.new(url: Settings.workflow_url, logger: logger, timeout: Settings.workflow.timeout)
+    Dor::Workflow::Client.new(url: Settings.workflow.url, logger: logger, timeout: Settings.workflow.timeout)
   end
 end
--- spec/indexers/workflow_indexer_spec.rb
+++ spec/indexers/workflow_indexer_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'webmock/rspec'
+# require 'webmock/rspec'
 
 RSpec.describe WorkflowIndexer do
   before do
